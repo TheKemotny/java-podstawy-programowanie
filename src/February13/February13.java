00b6c7d2216d882d47938662a15d426532478e6f
Tomasz Szyperek <tomek.szyperek@gmail.com>
2023-02-13 20:59:48 +0100

    +task four

--- src/February13/February13.java
+++ src/February13/February13.java
@@ -22,26 +22,38 @@ public class February13 {
         double bmi = weight / Math.pow((height / 100f),2);
         System.out.println(bmi);
         if((bmi >= 18.5d) && (bmi <= 24.9)){
-            System.out.println("BMI optymalne");
+            System.out.println("BMI OK");
         }else{
-            System.out.println("BMI nie optymalne");
+            System.out.println("BMI not OK");
         }
     }
 
-    public static void taskExtra(String... array){
-        if(array.length == 0 || array[0].isEmpty()){
-            System.out.println("NO STRINGS OR STRING EMPTY");
+    //W podanym ciągu znaków znajdź najdłuższy podciąg składający się z jednego znaku np. AABCDDBBBEA -> “B:3”
+    //Ciąg to tablica Stringów dowolnej wielkości. Nie używaj w zadaniu Kolekcji (Collections tzn List, Setów itp.).
+    public static void taskExtra(String... array) {
+        if(array.length == 0) {
+            System.out.println("NO STRINGS");
+            return;
+        }
+        int firstNotEmpty = -1;
+        for(int i = 0; i < array.length; i++) {
+            if(!array[i].isEmpty()) {
+                firstNotEmpty = i;
+            }
+        }
+        if(firstNotEmpty == -1){
+            System.out.println("ALL EMPTY STRINGS");
             return;
         }
         int currentLongestCounterOfCharacter = -1;
-        int currentCharacterCounter = 1;
-        char currentChar = array[0].charAt(0);
-        char longestChar = array[0].charAt(0);
-        for(int i = 0; i < array.length; i++){
-            for(int j = 0; j < array[i].length(); j++){
-                if(currentChar == array[i].charAt(j)){
+        int currentCharacterCounter = 0;
+        char currentChar = array[firstNotEmpty].charAt(0);
+        char longestChar = array[firstNotEmpty].charAt(0);
+        for(int i = firstNotEmpty; i < array.length; i++) {
+            for(int j = 0; j < array[i].length(); j++) {
+                if(currentChar == array[i].charAt(j)) {
                     currentCharacterCounter++;
-                }else{
+                }else {
                     if(currentLongestCounterOfCharacter < currentCharacterCounter){
                         currentLongestCounterOfCharacter = currentCharacterCounter;
                         longestChar = currentChar;
@@ -51,16 +63,14 @@ public class February13 {
                 }
             }
         }
-        if(currentLongestCounterOfCharacter < currentCharacterCounter){
+        if(currentLongestCounterOfCharacter < currentCharacterCounter) {
             currentLongestCounterOfCharacter = currentCharacterCounter;
             longestChar = currentChar;
         }
-        System.out.println("LONGEST CHAIN OF CHARACTER IS: \'" + longestChar + "\' WITH " + currentLongestCounterOfCharacter + " times");
+        System.out.println("LONGEST CHAIN OF CHARACTER IS: '" + longestChar + "' WITH " + currentLongestCounterOfCharacter + " times");
         System.out.println(longestChar + ":" + currentLongestCounterOfCharacter);
     }
 
-    //W podanym ciągu znaków znajdź najdłuższy podciąg składający się z jednego znaku np. AABCDDBBBEA -> “B:3”
-    //Ciąg to tablica Stringów dowolnej wielkości. Nie używaj w zadaniu Kolekcji (Collections tzn List, Setów itp.).
     public static void taskThree(){
         Scanner scanner = new Scanner(System.in);
         System.out.println("A: ");
@@ -71,23 +81,43 @@ public class February13 {
         int c = scanner.nextInt();
 
         double delta = Math.pow(b,2) - (4 * a * c);
+
         if(delta < 0) {
             System.out.println("Delta ujemna");
         } else if (delta == 0) {
-            float x = (-b) / (2*a);
+            double x = (-b) / (2d*a);
             System.out.println("X0 = " + x);
         } else {
-            float x1 = (float) ((-b-Math.sqrt(delta))/(2*a));
-            float x2 = (float) ((-b+Math.sqrt(delta))/(2*a));
+            double x1 = (-b-Math.sqrt(delta))/(2*a);
+            double x2 = (-b+Math.sqrt(delta))/(2*a);
             System.out.println("X1 = " + x1 + "\nX2 = " + x2);
+        }
+    }
+
+    public static void taskFour(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("NUMBER: ");
+        int userInput = scanner.nextInt();
+        for(int i = 1; i <= userInput; i++){
+            System.out.print(i);
+            if(i % 3 == 0 && i % 7 == 0) {
+                System.out.print(" Pif paf");
+            } else if (i % 3 == 0) {
+                System.out.print(" Pif");
+            } else if (i % 7 == 0) {
+                System.out.print(" Paf");
+            }
+            System.out.println();
         }
     }
 
     public static void main(String[] args) {
         //taskExtra("");
         //taskExtra();
-        //taskExtra("aaa","bb","+++++++","dijhasohfas","123355555555");
+        //taskExtra("","bb","+++++++","dijhasohfas","123355555555");
         //taskExtra("asfasfm,", "ala ma kota", "          ");
-        taskThree();
+        //taskExtra("AABCDDBBBEA");
+        //taskThree();
+        //taskFour();
     }
 }
